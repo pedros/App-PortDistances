f851e0d0daad4f6238a8db7c74e13eaa3ab6a964
Pedro Silva <pedros@berkeley.edu>
2010-07-13 03:08:22 -0700

    * use MooseX::Declare * build new Graph based on edges from apsp/sssp     - return new Graph instead of homebrew data structure * add distance_matrix($G)     - return new Graph::Matrix of apsp/sssp (docs say its meant for internal use only)

--- lib/App/PortDistances/Graph.pm
+++ lib/App/PortDistances/Graph.pm
@@ -1,111 +1,131 @@
-package App::PortDistances::Graph;
+use MooseX::Declare;
 
-use Graph::Directed;
-use FindBin qw//;
+class App::PortDistances::Graph {
 
-use Moose;
-use MooseX::Method::Signatures;
+    use Graph;
+    use FindBin;
 
-use Data::Dumper;
-use lib File::Spec->catdir( $FindBin::Bin, ('..') x 3, 'lib' );
+    use Data::Dumper;
+    use lib File::Spec->catdir( $FindBin::Bin, ('..') x 3, 'lib' );
 
-use App::PortDistances::DB;
+    use App::PortDistances::DB;
 
-has '_db' => (
-    is       => 'ro',
-    isa      => 'App::PortDistances::DB',
-    default  => sub { App::PortDistances::DB->new },
-    lazy     => 1,
-    required => 1,
-    clearer  => '_clear__db',
-);
+    has '_db' => (
+        is       => 'ro',
+        isa      => 'App::PortDistances::DB',
+        default  => sub { App::PortDistances::DB->new },
+        lazy     => 1,
+        required => 1,
+        clearer  => '_clear__db',
+    );
 
-has 'graph' => (
-    is       => 'ro',
-    isa      => 'Graph',
-    builder  => '_build_graph',
-    lazy     => 1,
-    handles => {
-        SPT_Dijkstra => 'SPT_Dijkstra',
-        APSP_Floyd_Warshall => 'APSP_Floyd_Warshall', 
-    }
-);
-
-sub _build_graph {
-    my ($self) = @_;
-
-    my $graph = Graph->new;
-
-SOURCE:
-    for my $source ( $self->_db->ports ) {
-        $graph->set_vertex_attribute( $source->name, 'latitude',  $source->latitude  );
-        $graph->set_vertex_attribute( $source->name, 'longitude', $source->longitude );
-
-    TARGET:
-        for my $target ( $self->_db->details($source->targets) ) {
-            next TARGET unless defined $target;
-            
-            $graph->set_vertex_attribute( $target->name, 'latitude',  $target->latitude  );
-            $graph->set_vertex_attribute( $target->name, 'longitude', $target->longitude );
-            $graph->add_weighted_path( $source->name, $source->distance($target), $target->name );
+    has 'graph' => (
+        is       => 'ro',
+        isa      => 'Graph',
+        builder  => '_build_graph',
+        lazy     => 1,
+        handles  => {
+            SPT_Dijkstra        => 'SPT_Dijkstra',
+            APSP_Floyd_Warshall => 'APSP_Floyd_Warshall', 
+            vertices            => 'vertices',
         }
-    }
-    
-    $self->_clear__db;
-    return $graph;
-}
+    );
 
-sub graph_details {
-    my ($self) = @_;
+    sub _build_graph {
+        my ($self) = @_;
 
-    my @density_limits = $self->density_limits;
+        my $graph = Graph->new;
+    SOURCE:
+        for my $source ( $self->_db->ports ) {
+            $graph->set_vertex_attribute( $source->name, 'latitude',  $source->latitude  );
+            $graph->set_vertex_attribute( $source->name, 'longitude', $source->longitude );
 
-    return density => $self->density,
-        edges      => scalar $self->edges,
-        vertices   => scalar $self->vertices,
-        sparse     => $density_limits[0],
-        dense      => $density_limits[1],
-        complete   => $density_limits[2];
-}
-
-method shortest_paths (ArrayRef[Str] :$sources? = [], ArrayRef[Str] :$targets? = [], Bool :$apsp? = 0) {
-    my ($SP, %paths);
-
-    $SP = $self->APSP_Floyd_Warshall if $apsp;
- SOURCE:
-    for my $source (@$sources ? @$sources : $self->vertices) {
-        my $SP = $self->SPT_Dijkstra($source) unless $apsp;
-
-    TARGET:
-        for my $target (@$targets ? @$targets : $SP->vertices) {
-
-            my $current = $target;
-        CURRENT:
-            while ( defined $current ) {
-                my $predecessor
-                    = $SP->get_vertex_attribute( $current, q{p} );
-
-                next TARGET unless $predecessor;
-
-                my $weight
-                    = $SP->get_edge_attribute( $predecessor, $current,
-                    'weight' );
-
-                unshift @{ $paths{$source}{$target} }, [ $current, $weight ];
-                $current = $predecessor;
+        TARGET:
+            for my $target ( $self->_db->details($source->targets) ) {
+                next TARGET unless defined $target; ## TODO: remove?
+            
+                $graph->set_vertex_attribute( $target->name, 'latitude',  $target->latitude  );
+                $graph->set_vertex_attribute( $target->name, 'longitude', $target->longitude );
+                $graph->add_weighted_path( $source->name, $source->distance($target->name), $target->name );
             }
         }
+            $self->_clear__db;
+        return $graph;
     }
 
-    return \%paths;
-}
+    method graph_details {
+        my @density_limits = $self->density_limits;
 
-no Moose;
-__PACKAGE__->meta->make_immutable;
-1;
+        return (density    => $self->density,
+                edges      => scalar $self->edges,
+                vertices   => scalar $self->vertices,
+                sparse     => $density_limits[0],
+                dense      => $density_limits[1],
+                complete   => $density_limits[2]);
+    }
 
-__END__
+    method shortest_paths (ArrayRef[Str] :$sources? = [], ArrayRef[Str] :$targets? = [], Bool :$apsp? = 0) {
+        my $G  = Graph->new;
+        my $SP = $self->APSP_Floyd_Warshall if $apsp;
+
+        #my %paths;
+    SOURCE:
+        for my $source (@$sources ? @$sources : $self->vertices) {
+            $SP = $self->SPT_Dijkstra( $source ) unless $apsp;
+
+        TARGET:
+            for my $target (@$targets ? @$targets : $SP->vertices) {
+
+                # TODO: refactor out
+                #     my $current = $target;
+                # CURRENT:
+                #     while ( defined $current ) {
+                #         my $predecessor
+                #             = $SP->get_vertex_attribute( $current, q{p} );
+
+                #         next TARGET unless $predecessor or $current;
+
+                #         my $weight
+                #             = $SP->get_edge_attribute( $predecessor, $current, 'weight' );
+
+                #         unshift @{ $paths{$source}{$target} }, [ $current, $weight ];
+                #         $current = $predecessor;
+                #     }
+
+                my $predecessor
+                    = $SP->get_vertex_attribute( $target, q{p} )
+                        || next TARGET;
+
+                my $weight
+                    = $SP->get_edge_attribute( $predecessor, $target, 'weight' );
+
+                $G->add_weighted_path(
+                    $source,
+                    $weight,
+                    $target
+                );
+            }
+        }
+        return $G;
+    }
+
+    method distance_matrix ( $G! ) {
+        my $M = Graph::Matrix->new( $G );
+    SOURCE:
+        for my $source ( $G->vertices ) {
+
+        TARGET:
+            for my $target ( $G->vertices ) {
+                my $weight
+                    = $G->get_edge_attribute( $source, $target, 'weight' );
+
+                $M->set( $source, $target, $weight );
+            }
+        }
+        return $M;
+    }
+};
+
 my $o = App::PortDistances::Graph->new;
-$o->graph;
-print Dumper $o->shortest_paths( apsp => 1, sources => ['Setubal, Portugal'] );exit;
+print Dumper $o->distance_matrix( $o->shortest_paths( apsp => 1 ) );exit;
 
