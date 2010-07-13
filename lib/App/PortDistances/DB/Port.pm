47be1fd4d2e595017ee80d7904a666657e801c38
Pedro Silva <pedros@berkeley.edu>
2010-07-13 02:54:34 -0700

    use MooseX::Declare

--- lib/App/PortDistances/DB/Port.pm
+++ lib/App/PortDistances/DB/Port.pm
@@ -1,28 +1,31 @@
-package App::PortDistances::DB::Port;
+use MooseX::Declare;
 
-use Moose;
+class App::PortDistances::DB::Port {
 
-use App::PortDistances::Types qw/Coord/;
+    use App::PortDistances::Types qw/Coord/;
 
-has [qw/name country/]       => ( is => 'ro', isa => 'Str', required => 1, lazy => 1, default => sub { shift->names->[-1] } );
-has [qw/latitude longitude/] => ( is => 'ro', isa => Coord, required => 1 );
-has [qw/names notes/]        => ( is => 'ro', isa => 'ArrayRef[Str]' );
-has 'note' => ( is => 'ro', isa => 'Str' );
+    has 'note'                   => ( is => 'ro', isa => 'Str'                );
+    has 'junction'               => ( is => 'ro', isa => 'Bool'               );
+    has [qw/latitude longitude/] => ( is => 'ro', isa => Coord, required => 1 );
+    has [qw/names notes/]        => ( is => 'ro', isa => 'ArrayRef[Str]'      );
 
-has 'ports' => (
-    is      => 'ro',
-    isa     => 'HashRef[Num]',
-    traits  => ['Hash'],
-    handles => {
-        targets  => 'keys',
-        size     => 'count',
-        connects => 'exists',
-        distance => 'get',
-    }
-);
-
-has 'junction' => ( is => 'ro', isa => 'Bool' );
-
-no Moose;
-__PACKAGE__->meta->make_immutable;
-1;
+    has [qw/name country/] => (
+        is       => 'ro',
+        isa      => 'Str',
+        required => 1,
+        lazy     => 1,
+        default  => sub { shift->names->[-1] }
+    );
+    
+    has 'ports' => (
+        is      => 'ro',
+        isa     => 'HashRef[Num]',
+        traits  => ['Hash'],
+        handles => {
+            targets     => 'keys',
+            connections => 'count',
+            connects    => 'exists',
+            distance    => 'get',
+        }
+    );
+};
