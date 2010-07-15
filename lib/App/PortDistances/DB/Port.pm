b80ccf8f077c4350e2e2545b9c66ce3efadb35e6
Pedro Silva <pedros@berkeley.edu>
2010-07-14 17:26:13 -0700

    minor commit

--- lib/App/PortDistances/DB/Port.pm
+++ lib/App/PortDistances/DB/Port.pm
@@ -26,6 +26,7 @@ class App::PortDistances::DB::Port {
             connections => 'count',
             connects    => 'exists',
             distance    => 'get',
+            delete      => 'delete',
         }
     );
 };
