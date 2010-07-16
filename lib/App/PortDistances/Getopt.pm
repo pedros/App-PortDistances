b384deb1a157af9b6ce09cc007b7a430cf3ac4b2
Pedro Silva <pedros@berkeley.edu>
2010-07-16 15:22:29 -0700

    add methods: _db() to hold DB structure and _cache() to hold Cache structure; add filter-db, filter-source and filter-target for delegating to DB::find() - probably need to be refactored into single method

--- lib/App/PortDistances/Getopt.pm
+++ lib/App/PortDistances/Getopt.pm
@@ -1,22 +1,20 @@
 use MooseX::Declare;
 
 class App::PortDistances::Getopt {
-    
+
+    use Data::Dumper;
     use FindBin;
     use lib File::Spec->catdir( $FindBin::Bin, ('..') x 3, 'lib' );
-    
+
     with 'MooseX::Getopt';
     use MooseX::Types::Moose qw/HashRef/;
 
     use App::PortDistances::Types qw/File StrArray/;
     use App::PortDistances::DB;
     use App::PortDistances::Cache;
-    
-    # MooseX::Getopt::OptionTypeMap->add_option_type_to_map('File' => '=s');
-    # MooseX::Getopt::OptionTypeMap->add_option_type_to_map('DB'   => '=s');
 
     has [qw/source target/] => (
-        is       => 'ro',
+        is       => 'rw',
         isa      => StrArray,
         coerce   => 1,
         required => 0,
@@ -27,25 +25,74 @@ class App::PortDistances::Getopt {
     has 'db' => (
         is            => 'ro',
         isa           => File,
-        required      => 0,
+        required      => 1,
         lazy          => 1,
         default       => sub { App::PortDistances::DB->DB_FILE },
         documentation => 'source database in JSON format',
     );
 
+    has '_db' => (
+        is       => 'rw',
+        isa      => 'App::PortDistances::DB',
+        required => 1,
+        lazy     => 1,
+        default =>
+            sub { App::PortDistances::DB->new( db_file => shift->db ) },
+    );
+
     has 'cache' => (
         is            => 'ro',
         isa           => File,
+        required      => 0,
         default       => sub { App::PortDistances::Cache->CACHE_ROOT },
         documentation => 'Cache directory',
     );
 
-    has 'filter' => (
-        is => 'ro',
-        isa => HashRef,
+    has '_cache' => (
+        is       => 'ro',
+        isa      => 'App::PortDistances::Cache',
+        required => 0,
+        lazy     => 1,
+        default  => sub {
+            App::PortDistances::Cache->new( cache_root => shift->cache );
+        },
+    );
+
+    has 'filter-db' => (
+        is      => 'ro',
+        isa     => HashRef,
+        trigger => sub {
+            my $self = shift;
+            my $db = App::PortDistances::DB->new( db_file => $self->db );
+            $db->db;
+            $self->_db( $db->find( %{ shift @_ } ) );
+        },
+    );
+
+    has 'filter-source' => (
+        is      => 'ro',
+        isa     => HashRef,
+        trigger => sub {
+            my $self = shift;
+            my $db = App::PortDistances::DB->new( db_file => $self->db );
+            $db->db;
+            $self->source( $db->find( %{ shift @_ } ) );
+        },
+    );
+
+    has 'filter-target' => (
+        is      => 'ro',
+        isa     => HashRef,
+        trigger => sub {
+            my $self = shift;
+            my $db = App::PortDistances::DB->new( db_file => $self->db );
+            $db->db;
+            $self->target( $db->find( %{ shift @_ } ) );
+        },
     );
 };
 
 use Data::Dumper;
 my $o = App::PortDistances::Getopt->new_with_options;
-print Dumper $o->db, $o->cache, $o->filter;
+$o->_db->db;
+print Dumper $o;
