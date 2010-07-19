f02a13c46d1c4d23fdb543a662204cb9ef3648e1
Pedro Silva <pedros@berkeley.edu>
2010-07-19 11:54:12 -0700

    add 'use' boolean attribute. change 'freeze' to 'set' and 'thaw' to 'get'. add 'in' for exists predicate.

--- lib/App/PortDistances/Cache.pm
+++ lib/App/PortDistances/Cache.pm
@@ -2,17 +2,17 @@ use MooseX::Declare;
 
 class App::PortDistances::Cache {
 
-    use File::Spec;
     use FindBin;
     use lib File::Spec->catdir( $FindBin::Bin, ('..') x 3, 'lib' );
-    use Cwd;
-    use constant CACHE_ROOT => File::Spec->catdir( $ENV{HOME}
-            || $ENV{HOMEPATH}
-            || File::Spec->curdir,
-        '.port_distances' );
 
+    use File::Spec;
+    use Cwd;
     use Cache::File;
     use Digest::MD5;
+    use constant CACHE_ROOT => File::Spec->catdir( $ENV{HOME}
+                                                || $ENV{HOMEPATH}
+                                                || File::Spec->curdir,
+                                                   '.port_distances' );
 
     use App::PortDistances::Types qw/File/;
 
@@ -29,25 +29,26 @@ class App::PortDistances::Cache {
         isa             => 'Cache::File',
         required        => 1,
         lazy            => 1,
-        builder         => '_build_cache',
-        clearer         => '_clear_cache',
+        default         => sub { Cache::File->new( cache_root=> shift->cache_root ) },
+        handles         => [qw/clear/],
     );
 
-    method _build_cache {
-        Cache::File->new(
-            cache_root => $self->cache_root,
-        );
-    };
+    has 'use' => ( is => 'ro', isa => 'Bool', required => 1, lazy => 1, default => 1 );
 
-    method _clear_cache {
-        $self->cache->clear;
-    };
-
-    method freeze ( $key!, $value! ) {
+    method set ( $key!, $value! ) {
+        return unless $self->use;
         $self->cache->freeze( Digest::MD5->new->add($key)->hexdigest, $value );
-    }
+    };
 
-    method thaw ( $key! ) {
+    method get ( $key! ) {
+        return unless $self->use;
         $self->cache->thaw( Digest::MD5->new->add($key)->hexdigest );
-    }
-}
+    };
+
+    method in ( $key! ) {
+        return unless $self->use;
+        $self->cache->exists( Digest::MD5->new->add($key)->hexdigest );
+    };
+};
+
+    
